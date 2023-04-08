4bdf2dab53cdde11a84dc24f8fac0840cb900e00
nishan gautam <nishangautam.ng@gmail.com>
2023-04-08 14:54:43 +1000

    updated css

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,14 +2,17 @@
   <section>
     <h2>Tasks</h2>
     <div class="task-container">
-      <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Enter a new task" />
+      <div class="input-container">
+        <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Enter a new task" />
+        <button @click="addTask">Add</button>
+      </div>
       <ul>
         <li v-for="(task, index) in tasks" :key="index">
           <div class="task-item">
             <input type="checkbox" @change="toggleCompleted(task)" :checked="task.completed" />
             <label :class="{ 'task-completed': task.completed }">{{ task.text }}</label>
           </div>
-          <button @click="deleteTask(index)">Delete</button>
+          <button class="delete-button" @click="deleteTask(index)">Delete</button>
         </li>
       </ul>
     </div>
@@ -37,8 +40,8 @@ export default {
       this.tasks.splice(taskIndex, 1);
     },
     toggleCompleted(task) {
-    task.completed = !task.completed;
-  },
+      task.completed = !task.completed;
+    },
   },
 };
 </script>
@@ -69,14 +72,32 @@ button {
 button:hover {
   background-color: #1c86ee;
 }
+
+.delete-button {
+  font-size: 14px;
+  padding: 6px 8px;
+  margin: 0 10px; /* Change from margin-left to margin */
+  cursor: pointer;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  background-color: #ff6961;
+  color: white;
+}
+
+.delete-button:hover {
+  background-color: #ff4136;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 .task-completed {
   text-decoration: line-through;
   color: red;
 }
+
 .task-item {
   display: flex;
   align-items: center;
@@ -85,16 +106,27 @@ ul {
 input[type="checkbox"] {
   margin-right: 10px;
 }
+
+.input-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
 .task-container {
   width: 50%;
   margin: 0 auto;
+  background-color: #f5f5f5; /* Add this line to give a subtle grey background */
+  padding: 20px;
+  border-radius: 4px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
 }
 
 @media screen and (max-width: 767px) {
-  .task-container {
-    width: 90%;
-  }
+.task-container {
+width: 90%;
 }
-
-
+}
 </style>
+

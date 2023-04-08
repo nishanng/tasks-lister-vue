44045133dfe0e4f178175d172eef628f7cbe1912
nishan gautam <nishangautam.ng@gmail.com>
2023-04-08 17:59:50 +1000

    readme update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/tasks-lister-vue/" : "/",
+};
+
+
 export default defineConfig({
   plugins: [vue()],
 });

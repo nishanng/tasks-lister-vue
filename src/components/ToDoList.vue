7f82acc97afb7d08be25a3d890ad704eea19f789
nishan gautam <nishangautam.ng@gmail.com>
2023-04-08 15:13:58 +1000

    changed format and style

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -5,19 +5,21 @@
       <div class="input-container">
         <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Enter a new task" />
         <button @click="addTask">Add</button>
+        <ul>
+          <li v-for="(task, index) in tasks" :key="index">
+            <div class="task-item">
+              <input type="checkbox" @change="toggleCompleted(task)" :checked="task.completed" />
+              <label :class="{ 'task-completed': task.completed }">{{ task.text }}</label>
+              <button class="delete-button" @click="deleteTask(index)">Delete</button>
+            </div>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li v-for="(task, index) in tasks" :key="index">
-          <div class="task-item">
-            <input type="checkbox" @change="toggleCompleted(task)" :checked="task.completed" />
-            <label :class="{ 'task-completed': task.completed }">{{ task.text }}</label>
-          </div>
-          <button class="delete-button" @click="deleteTask(index)">Delete</button>
-        </li>
-      </ul>
+      <button v-if="showDeleteAllButton" class="delete-all-button" @click="deleteAllTasks">Delete All</button>
     </div>
   </section>
 </template>
+
 
 <script>
 export default {
@@ -27,6 +29,11 @@ export default {
       tasks: [],
       newTask: "",
     };
+  },
+  computed: {
+    showDeleteAllButton() {
+      return this.tasks.length > 1;
+    },
   },
   methods: {
     addTask() {
@@ -38,6 +45,9 @@ export default {
     },
     deleteTask(taskIndex) {
       this.tasks.splice(taskIndex, 1);
+    },
+    deleteAllTasks() {
+      this.tasks = [];
     },
     toggleCompleted(task) {
       task.completed = !task.completed;
@@ -57,11 +67,22 @@ input[type="text"] {
   border: 1px solid #ccc;
 }
 
+ul {
+  list-style-type: none; /* Add this line to remove bullet points */
+  padding: 0;
+  margin: 0; /* Add this line to remove the default margin */
+}
+
+li {
+  display: flex; /* Add this line to align items in a row */
+  align-items: center; /* Add this line to vertically align items */
+}
+
 button {
   font-size: 16px;
   padding: 8px 12px;
   margin-bottom: 10px;
-  margin-left: 10px; /* Add this line to create space between the input field and button */
+  margin-left: 10px;
   cursor: pointer;
   border-radius: 4px;
   border: 1px solid #ccc;
@@ -76,7 +97,6 @@ button:hover {
 .delete-button {
   font-size: 14px;
   padding: 6px 8px;
-  margin: 0 10px; /* Change from margin-left to margin */
   cursor: pointer;
   border-radius: 4px;
   border: 1px solid #ccc;
@@ -88,45 +108,15 @@ button:hover {
   background-color: #ff4136;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.task-completed {
-  text-decoration: line-through;
-  color: red;
-}
-
-.task-item {
-  display: flex;
-  align-items: center;
-}
-
-input[type="checkbox"] {
-  margin-right: 10px;
-}
-
-.input-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 10px;
-}
-
-.task-container {
-  width: 50%;
-  margin: 0 auto;
-  background-color: #f5f5f5; /* Add this line to give a subtle grey background */
-  padding: 20px;
+.delete-all-button {
+  font-size: 16px;
+  padding: 8px 12px;
+  margin-top: 10px;
+  cursor: pointer;
   border-radius: 4px;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-}
-
-@media screen and (max-width: 767px) {
-.task-container {
-width: 90%;
-}
+  border: 1px solid #ccc;
+  background-color: #dc143c;
+  color: white;
 }
 </style>
 

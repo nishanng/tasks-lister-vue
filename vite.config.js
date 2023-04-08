e6470eca87cce97d9bff46d9286b62befbd0a2f7
nishan gautam <nishangautam.ng@gmail.com>
2023-04-08 18:02:14 +1000

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/tasks-lister-vue/" : "/",
-};
-
-
 export default defineConfig({
   plugins: [vue()],
 });
